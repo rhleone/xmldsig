b6e6903f24b70d231b3b636517cef2f71190a9c9
Roger Leon <rhleone@gmail.com>
2023-02-12 11:24:55 -0400

    removemos formatoOut

--- src/XmlSigner.php
+++ src/XmlSigner.php
@@ -101,7 +101,6 @@ final class XmlSigner
      */
     private function appendSignature(DOMDocument $xml, string $digestValue): void
     {
-        $xml->formatOutput = true;
         $signatureElement = $xml->createElement('Signature');
         $signatureElement->setAttribute('xmlns', 'http://www.w3.org/2000/09/xmldsig#');
 
